ac4571c2f5ff84617ae11baf51f2e6c3c1151186
Victória Freyre <168319066+VicFreyre@users.noreply.github.com>
2025-08-20 17:29:14 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import cv2
+
+caminho = r"C:\Users\ruvic\Desktop\ml\orchid.jpg"
+
+img_colorida = cv2.imread(caminho)
+
+img_cinza = cv2.cvtColor(img_colorida, cv2.COLOR_BGR2GRAY)
+
+_, img_binaria = cv2.threshold(img_cinza, 127, 255, cv2.THRESH_BINARY)
+
+cv2.imshow("Imagem Original", img_colorida)
+cv2.imshow("Imagem em Cinza", img_cinza)
+cv2.imshow("Imagem Binaria", img_binaria)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
